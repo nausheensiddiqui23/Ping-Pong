10137b731a44891ba1da42e0d5f1cc4121a64c6a
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-10 23:23:10 +0530

    new commit

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
+
+const scoreLeftEl = document.getElementById('scoreLeft');
+const scoreRightEl = document.getElementById('scoreRight');
+const startBtn = document.getElementById('startBtn');
+const pauseBtn = document.getElementById('pauseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const modeSelect = document.getElementById('modeSelect');
+const aiRange = document.getElementById('aiRange');
